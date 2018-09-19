375953e879e8b83f085c735f03973c91fcb41ffd
Jorge Saborido <jorge.saborido@netexlearning.com>
2018-09-19 16:13:08 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ntx-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ux-components';
+}
