67b24fc42aa47c0f52653d19524b04dd8f38ff72
Jorge Saborido <jorge.saborido@netexlearning.com>
2018-10-03 17:05:15 +0200

    Eliminado de app.module.ts la importación de BrowserAnimationsModule que ya está en shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -12,7 +11,6 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
     CoreModule,
     AppRoutingModule
   ],
