709e7784c09d85ac09279116766a2628b2968674
Maria Roade <maria.roade@netexlearning.com>
2018-10-24 09:39:08 +0200

    Añadir los css a cada componente y quitarlos del styles.css, añadir la etiqueta del componente al lado de cada componente

--- projects/netex/ux-components/src/lib/ntx-toolbar/toolbar-tabs/toolbar-tabs.component.ts
+++ projects/netex/ux-components/src/lib/ntx-toolbar/toolbar-tabs/toolbar-tabs.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'ntx-toolbar-tabs',
   templateUrl: './toolbar-tabs.component.html',
-  styleUrls: ['./toolbar-tabs.component.scss']
+  styleUrls: ['./toolbar-tabs.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ToolbarTabsComponent implements OnInit {
 
