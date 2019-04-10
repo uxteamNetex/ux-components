68f6e17627ebc0ac6b2eb16a09e00aa3c6de033c
jaime.alonso <jaime.alonso@netexlearning.com>
2019-04-10 08:57:24 +0200

    [BUG] - Componente Card11 -Selección de card sólo cuando el radiobutton está seleccionado -Sombreado de card mediante propiedad box-shadow

--- projects/netex/ux-components/src/lib/ntx-cards/card11/card11.component.ts
+++ projects/netex/ux-components/src/lib/ntx-cards/card11/card11.component.ts
@@ -1,26 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { ViewEncapsulation } from '@angular/core';
 import { Input } from '@angular/core';
+import { MatRadioChange } from '@angular/material';
 
 @Component({
-  selector: 'ntx-card11',
-  templateUrl: './card11.component.html',
-  styleUrls: ['./card11.component.scss'],
-  encapsulation: ViewEncapsulation.None
+	selector: 'ntx-card11',
+	templateUrl: './card11.component.html',
+	styleUrls: ['./card11.component.scss'],
+	encapsulation: ViewEncapsulation.None
 })
 export class Card11Component implements OnInit {
 
-  status = false;
+	data: any = {
+		title: 'Card title lorem ipsum dolor sit amet consectetuer adipiscing',
+		description: 'Card subtitle lorem ipsum dolor sit amet consectetuer lorem ipsum dolor sit amet consectetuer',
+		rank: '6'
+	};
 
-  @Input() color: string;
+	@Input() color: string;
+	@Input() value: number;
+	@Input() selected: boolean;
 
-  onChange() {
-    this.status = !this.status;
-  }
+	@Output() radiobuttonChange = new EventEmitter<MatRadioChange>();
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
+
+	onChangeRadiobutton(event: MatRadioChange) {
+		this.radiobuttonChange.emit(event);
+	}
 
 }
