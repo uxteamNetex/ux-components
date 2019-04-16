966e173fd514236979578bec3cc34162439fc86e
jaime.alonso <jaime.alonso@netexlearning.com>
2019-04-16 17:26:55 +0200

    [BUG] - Cabecera de selección de tablas -Permitir selección individual de filas. -Parametrizar mensaje (singular/plural) -Usar componente ntx-text-button

--- projects/netex/ux-components/src/lib/ntx-tables/selectable-table/selectable-table.component.ts
+++ projects/netex/ux-components/src/lib/ntx-tables/selectable-table/selectable-table.component.ts
@@ -1,101 +1,74 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
 import { SelectionModel } from '@angular/cdk/collections';
-import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ViewEncapsulation } from '@angular/core';
 import { Input } from '@angular/core';
+import { MatPaginator, MatSort, MatTableDataSource, PageEvent } from '@angular/material';
 
 export interface PeriodicElement {
-  name: string;
-  project: string;
-  modification: string;
+	name: string;
+	project: string;
+	modification: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  // tslint:disable-next-line:max-line-length
-  {name: 'Element name lorem ipsum dolor sit amet consectetur adipiscing elit quam risus pretium', project: 'Project name lorem ipsum dolor sit amet consectetur adipiscing elit quam risus pretium',  modification: '29/11/2018'},
-  // tslint:disable-next-line:max-line-length
-  {name: 'Element name lorem ipsum dolor sit amet consectetur adipiscing elit quam risus pretium', project: 'Project name lorem ipsum dolor sit amet consectetur adipiscing elit quam risus pretium',  modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'},
-  {name: 'Element name', project: 'Project name', modification: '29/11/2018'}
+	// tslint:disable-next-line:max-line-length
+	{name: 'Element name lorem ipsum dolor sit amet consectetur adipiscing elit quam risus pretium', project: 'Project name lorem ipsum dolor sit amet consectetur adipiscing elit quam risus pretium',  modification: '29/11/2018'},
+	// tslint:disable-next-line:max-line-length
+	{name: 'Element name lorem ipsum dolor sit amet consectetur adipiscing elit quam risus pretium', project: 'Project name lorem ipsum dolor sit amet consectetur adipiscing elit quam risus pretium',  modification: '29/11/2018'},
+	{name: 'Element name', project: 'Project name', modification: '29/11/2018'},
+	{name: 'Element name', project: 'Project name', modification: '29/11/2018'},
+	{name: 'Element name', project: 'Project name', modification: '29/11/2018'},
+	{name: 'Element name', project: 'Project name', modification: '29/11/2018'},
+	{name: 'Element name', project: 'Project name', modification: '29/11/2018'},
+	{name: 'Element name', project: 'Project name', modification: '29/11/2018'},
+	{name: 'Element name', project: 'Project name', modification: '29/11/2018'},
+	{name: 'Element name', project: 'Project name', modification: '29/11/2018'}
 ];
 
 
+
 @Component({
-  selector: 'ntx-selectable-table',
-  templateUrl: './selectable-table.component.html',
-  styleUrls: ['./selectable-table.component.scss'],
-  encapsulation: ViewEncapsulation.None
+	selector: 'ntx-selectable-table',
+	templateUrl: './selectable-table.component.html',
+	styleUrls: ['./selectable-table.component.scss'],
+	encapsulation: ViewEncapsulation.None
 })
 export class SelectableTableComponent implements OnInit {
 
-  show = false;
-  checkboxValue: boolean;
+	constructor() { }
 
-  @Input() color: string;
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
+	displayedColumns: string[] = ['select', 'name', 'project', 'type', 'modification', 'options'];
+	dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+	selection = new SelectionModel<PeriodicElement>(true, []);
 
-  displayedColumns: string[] = ['select', 'name', 'project', 'type', 'modification', 'options'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-  selection = new SelectionModel<PeriodicElement>(true, []);
+	@ViewChild(MatPaginator) paginator: MatPaginator;
+	@ViewChild(MatSort) sort: MatSort;
 
-  showMultiselecToolbar() {
-    if (!this.checkboxValue) {
-      this.show = false;
-    } else {
-      this.show = true;
-    }
-  }
+	ngOnInit() {
+		this.dataSource.sort = this.sort;
+		this.dataSource.paginator = this.paginator;
+	}
 
-  /** Whether the number of selected elements matches the total number of rows. */
-  isAllSelected() {
-    const numSelected = this.selection.selected.length;
-    const numRows = this.dataSource.data.length;
-    return numSelected === numRows;
-  }
+	/** Whether the number of selected elements matches the total number of rows. */
+	isAllSelected() {
+		const numSelected = this.selection.selected.length;
+		const numRows = this.dataSource.data.length;
+		return numSelected === numRows;
+	}
 
-  /** Selects all rows if they are not all selected; otherwise clear selection. */
-  masterToggle() {
-    this.isAllSelected() ?
-        this.selection.clear() :
-        this.dataSource.data.forEach(row => this.selection.select(row));
-  }
+	/** Selects all rows if they are not all selected; otherwise clear selection. */
+	masterToggle() {
+		this.isAllSelected() ?
+			this.selection.clear() :
+			this.dataSource.data.forEach(row => this.selection.select(row));
+	}
 
-  constructor() { }
+	onRowCheckboxChange($event, row) {
+		return $event ? this.selection.toggle(row) : null;
+	}
 
-  ngOnInit() {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
-  }
+	onHeaderCheckboxChange($event) {
+		return $event ? this.masterToggle() : null;
+	}
 
 }
