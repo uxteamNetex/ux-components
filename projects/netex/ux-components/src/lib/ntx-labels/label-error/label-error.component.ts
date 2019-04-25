d39adae813257fb333987e486f20966c21659c0e
jaime.alonso <jaime.alonso@netexlearning.com>
2019-04-25 11:36:35 +0200

    [BUG] - Funcionamiento incorr...  tabla evaluation -Refactorizado html, scss y ts -Permitir selección individual/total de filas. -Corrección ancho cabecera de selección. -Parametrización componente 'ntx-label-error' -No utilización componente multiselección

--- projects/netex/ux-components/src/lib/ntx-labels/label-error/label-error.component.ts
+++ projects/netex/ux-components/src/lib/ntx-labels/label-error/label-error.component.ts
@@ -1,17 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'ntx-label-error',
-  templateUrl: './label-error.component.html',
-  styleUrls: ['./label-error.component.scss'],
-  encapsulation: ViewEncapsulation.None
+	selector: 'ntx-label-error',
+	templateUrl: './label-error.component.html',
+	styleUrls: ['./label-error.component.scss'],
+	encapsulation: ViewEncapsulation.None
 })
 export class LabelErrorComponent implements OnInit {
 
-  constructor() { }
+	@Input() text: string;
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {}
 
 }
--- projects/netex/ux-components/src/lib/ntx-tables/evaluation-table/evaluation-table.component.ts
+++ projects/netex/ux-components/src/lib/ntx-tables/evaluation-table/evaluation-table.component.ts
@@ -1,110 +1,98 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
 import { SelectionModel } from '@angular/cdk/collections';
-import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ViewEncapsulation } from '@angular/core';
 import { Input } from '@angular/core';
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 
 export interface PeriodicElement {
-  receivers: string;
-  job: string;
-  superiors: string;
-  pairs: string;
-  subordinates: string;
+	avatar: string;
+	evaluated: boolean;
+	receiver: string;
+	job: string;
+	alertMsg: string;
+	superiors: string;
+	pairs: string;
+	subordinates: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  // tslint:disable-next-line:max-line-length
-  {receivers: 'Amanda Matterson Cooper Amanda Matterson Cooper', job: 'Web developer Web developer Web developer', superiors: '2', pairs: '10', subordinates: '3'},
-  {receivers: 'Angeline Kerber Peterson', job: 'Marketing assistant', superiors: '1', pairs: '23', subordinates: '14'},
-  {receivers: 'Amanda Matterson Cooper', job: 'Web developer', superiors: '2', pairs: '10', subordinates: '3'},
-  {receivers: 'Angeline Kerber', job: 'Marketing assistant', superiors: '1', pairs: '23', subordinates: '14'},
-  {receivers: 'Amanda Matterson Cooper', job: 'Web developer', superiors: '2', pairs: '10', subordinates: '3'},
-  {receivers: 'Angeline Kerber Peterson', job: 'Marketing assistant', superiors: '1', pairs: '23', subordinates: '14'},
-  {receivers: 'Amanda Matterson', job: 'Web developer', superiors: '2', pairs: '10', subordinates: '3'},
-  {receivers: 'Angeline Kerber Peterson', job: 'Marketing assistant', superiors: '1', pairs: '23', subordinates: '14'},
-  {receivers: 'Amanda Matterson', job: 'Web developer', superiors: '2', pairs: '10', subordinates: '3'},
-  {receivers: 'Angeline Kerber Peterson', job: 'Marketing assistant', superiors: '1', pairs: '23', subordinates: '14'},
-  {receivers: 'Amanda Matterson Cooper', job: 'Web developer', superiors: '2', pairs: '10', subordinates: '3'},
-  {receivers: 'Angeline Kerber Peterson', job: 'Marketing assistant', superiors: '1', pairs: '23', subordinates: '14'},
-  {receivers: 'Amanda Matterson Cooper', job: 'Web developer Web developer', superiors: '2', pairs: '10', subordinates: '3'},
-  {receivers: 'Angeline Kerber Peterson', job: 'Marketing assistant', superiors: '1', pairs: '23', subordinates: '14'},
-  {receivers: 'Amanda Matterson Cooper', job: 'Web developer', superiors: '2', pairs: '10', subordinates: '3'},
-  {receivers: 'Angeline Kerber', job: 'Marketing assistant', superiors: '1', pairs: '23', subordinates: '14'},
-  {receivers: 'Amanda Matterson Cooper', job: 'Web developer', superiors: '2', pairs: '10', subordinates: '3'},
-  {receivers: 'Angeline Kerber Peterson', job: 'Marketing assistant', superiors: '1', pairs: '23', subordinates: '14'},
-  {receivers: 'Amanda Matterson', job: 'Web developer', superiors: '2', pairs: '10', subordinates: '3'},
-  {receivers: 'Angeline Kerber Peterson', job: 'Marketing assistant', superiors: '1', pairs: '23', subordinates: '14'},
-  {receivers: 'Amanda Matterson', job: 'Web developer', superiors: '2', pairs: '10', subordinates: '3'},
-  {receivers: 'Angeline Kerber Peterson', job: 'Marketing assistant', superiors: '1', pairs: '23', subordinates: '14'},
-  {receivers: 'Amanda Matterson Cooper', job: 'Web developer', superiors: '2', pairs: '10', subordinates: '3'},
-  {receivers: 'Angeline Kerber Peterson', job: 'Marketing assistant', superiors: '1', pairs: '23', subordinates: '14'},
-  {receivers: 'Amanda Matterson', job: 'Web developer', superiors: '2', pairs: '10', subordinates: '3'},
-  {receivers: 'Angeline Kerber Peterson', job: 'Marketing assistant', superiors: '1', pairs: '23', subordinates: '14'},
-  {receivers: 'Amanda Matterson', job: 'Web developer', superiors: '2', pairs: '10', subordinates: '3'},
-  {receivers: 'Angeline Kerber Peterson', job: 'Marketing assistant', superiors: '1', pairs: '23', subordinates: '14'},
-  {receivers: 'Amanda Matterson Cooper', job: 'Web developer', superiors: '2', pairs: '10', subordinates: '3'},
-  {receivers: 'Angeline Kerber Peterson', job: 'Marketing assistant', superiors: '1', pairs: '23', subordinates: '14'},
-  {receivers: 'Amanda Matterson Cooper', job: 'Web developer Web developer', superiors: '2', pairs: '10', subordinates: '3'},
-  {receivers: 'Angeline Kerber Peterson', job: 'Marketing assistant', superiors: '1', pairs: '23', subordinates: '14'},
-  {receivers: 'Amanda Matterson Cooper', job: 'Web developer', superiors: '2', pairs: '10', subordinates: '3'},
-  {receivers: 'Angeline Kerber', job: 'Marketing assistant', superiors: '1', pairs: '23', subordinates: '14'},
-  {receivers: 'Amanda Matterson Cooper', job: 'Web developer', superiors: '2', pairs: '10', subordinates: '3'},
-  {receivers: 'Angeline Kerber Peterson', job: 'Marketing assistant', superiors: '1', pairs: '23', subordinates: '14'},
-  {receivers: 'Amanda Matterson', job: 'Web developer', superiors: '2', pairs: '10', subordinates: '3'},
-  {receivers: 'Angeline Kerber Peterson', job: 'Marketing assistant', superiors: '1', pairs: '23', subordinates: '14'},
-  {receivers: 'Amanda Matterson', job: 'Web developer', superiors: '2', pairs: '10', subordinates: '3'},
-  {receivers: 'Angeline Kerber Peterson', job: 'Marketing assistant', superiors: '1', pairs: '23', subordinates: '14'},
-  {receivers: 'Amanda Matterson Cooper', job: 'Web developer', superiors: '2', pairs: '10', subordinates: '3'},
-  {receivers: 'Angeline Kerber Peterson', job: 'Marketing assistant', superiors: '1', pairs: '23', subordinates: '14'}
+	{ avatar: 'assets/images/perfil.jpg', evaluated: false, receiver: 'Amanda Matterson Cooper Amanda Matterson Cooper', job: 'Web developer', alertMsg: '', superiors: '2', pairs: '10', subordinates: '3'},
+	{ avatar: 'assets/images/perfil.jpg', evaluated: true, receiver: 'Amanda Matterson Cooper', job: 'Marketing assistant', alertMsg: '2', superiors: '2', pairs: '10', subordinates: '3'},
+	{ avatar: 'assets/images/perfil.jpg', evaluated: false, receiver: 'Angeline Kerber', job: 'Web developer', alertMsg: '3', superiors: '2', pairs: '10', subordinates: '3'},
+	{ avatar: 'assets/images/perfil.jpg', evaluated: true, receiver: 'Amanda Matterson', job: 'UX designer', alertMsg: '', superiors: '1', pairs: '5', subordinates: '3'},
+	{ avatar: 'assets/images/perfil.jpg', evaluated: true, receiver: 'Amanda Matterson', job: 'Database administrator', alertMsg: '2', superiors: '2', pairs: '2', subordinates: '3'},
+	{ avatar: 'assets/images/perfil.jpg', evaluated: false, receiver: 'Amanda Matterson', job: 'Web developer', alertMsg: '1', superiors: '2', pairs: '3', subordinates: '4'},
+	{ avatar: 'assets/images/perfil.jpg', evaluated: true, receiver: 'Amanda Matterson', job: 'Graphic designer', alertMsg: '5', superiors: '1', pairs: '2', subordinates: '2'},
+	{ avatar: 'assets/images/perfil.jpg', evaluated: false, receiver: 'Angeline Kerber', job: 'Web designer', alertMsg: '1', superiors: '2', pairs: '1', subordinates: '1'},
+	{ avatar: 'assets/images/perfil.jpg', evaluated: true, receiver: 'Amanda Matterson', job: 'Web designer', alertMsg: '3', superiors: '1', pairs: '2', subordinates: '4'},
+	{ avatar: 'assets/images/perfil.jpg', evaluated: true, receiver: 'Amanda Matterson', job: 'Web designer', alertMsg: '3', superiors: '1', pairs: '2', subordinates: '4'}
 ];
 
-
 @Component({
-  selector: 'ntx-evaluation-table',
-  templateUrl: './evaluation-table.component.html',
-  styleUrls: ['./evaluation-table.component.scss'],
-  encapsulation: ViewEncapsulation.None
+	selector: 'ntx-evaluation-table',
+	templateUrl: './evaluation-table.component.html',
+	styleUrls: ['./evaluation-table.component.scss'],
+	encapsulation: ViewEncapsulation.None
 })
-export class EvaluationTableComponent implements OnInit {
+export class EvaluationTableComponent implements OnInit, AfterViewInit {
 
-  show = false;
-  checkboxValue: boolean;
+	displayedColumns: string[] = ['receivers', 'superiors', 'pairs', 'subordinates'];
+	dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+	selection = new SelectionModel<PeriodicElement>(true, []);
 
-  @Input() color: string;
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
+	selectionHeaderStyle: object;
 
-  displayedColumns: string[] = ['select', 'receivers', 'superiors', 'pairs', 'subordinates'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-  selection = new SelectionModel<PeriodicElement>(true, []);
+	@ViewChild(MatPaginator) paginator: MatPaginator;
+	@ViewChild(MatSort) sort: MatSort;
+	@ViewChild('tableContainer') tableContainer: ElementRef;
 
-  showMultiselecToolbarEvaluation() {
-    if (!this.checkboxValue) {
-      this.show = false;
-    } else {
-      this.show = true;
-    }
-  }
+	itemPluralMapping = {
+		'=0': '0 items',
+		'=1': '1 item' ,
+		'other': '# items'
+	};
 
-  /** Whether the number of selected elements matches the total number of rows. */
-  isAllSelected() {
-    const numSelected = this.selection.selected.length;
-    const numRows = this.dataSource.data.length;
-    return numSelected === numRows;
-  }
+	alertPluralMapping = {
+		'=1': '1 alert' ,
+		'other': '# alerts'
+	};
 
-  /** Selects all rows if they are not all selected; otherwise clear selection. */
-  masterToggle() {
-    this.isAllSelected() ?
-        this.selection.clear() :
-        this.dataSource.data.forEach(row => this.selection.select(row));
-  }
+	constructor() { }
 
-  constructor() { }
+	ngOnInit() {
+		this.dataSource.sort = this.sort;
+		this.dataSource.paginator = this.paginator;
+	}
 
-  ngOnInit() {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
-  }
+	ngAfterViewInit() {
+		const flexBasis = this.tableContainer.nativeElement.offsetWidth - 16;
+		this.selectionHeaderStyle = {
+			'flex':  '0 0 ' + flexBasis + 'px',
+			'left': 0,
+			'position': 'sticky'
+		};
+	}
+
+	/** Whether the number of selected elements matches the total number of rows. */
+	isAllSelected() {
+		const numSelected = this.selection.selected.length;
+		const numRows = this.dataSource.data.length;
+		return numSelected === numRows;
+	}
+
+	/** Selects all rows if they are not all selected; otherwise clear selection. */
+	masterToggle() {
+		this.isAllSelected() ?
+				this.selection.clear() :
+				this.dataSource.data.forEach(row => this.selection.select(row));
+	}
+
+	onRowCheckboxChange($event, row) {
+		return $event ? this.selection.toggle(row) : null;
+	}
+
+	onHeaderCheckboxChange($event) {
+		return $event ? this.masterToggle() : null;
+	}
 
 }
