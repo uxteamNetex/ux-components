ba91d6999d0e89770a3a42c13bb12363002c22c6
Jaime Alonso Fernández <jaime.alonso@netexlearning.com>
2020-04-13 16:02:44 +0200

    Parametrización componente 'ntx-list-one-line'

--- src/app/ui-pages/lists/examples/lists-examples.component.ts
+++ src/app/ui-pages/lists/examples/lists-examples.component.ts
@@ -1,16 +1,55 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
+export interface Item {
+	label: string;
+	avatar: string;
+	color: string;
+}
+
 @Component({
-  selector: 'app-lists-examples',
-  templateUrl: './lists-examples.component.html',
-  styleUrls: ['./lists-examples.component.scss'],
-  encapsulation: ViewEncapsulation.None
+	selector: 'app-lists-examples',
+	templateUrl: './lists-examples.component.html',
+	styleUrls: ['./lists-examples.component.scss'],
+	encapsulation: ViewEncapsulation.None
 })
 export class ListsExamplesComponent implements OnInit {
 
-  constructor() { }
+	list: Item[] = [
+		{
+			label: 'Customer oriented',
+			avatar: undefined,
+			color: '#E7575B'
+		},
+		{
+			label: 'Leadership',
+			avatar: 'assets/images/app-catalogue.png',
+			color: '#5cce88'
+		},
+		{
+			label: 'Organisation',
+			avatar: 'assets/images/app-community.png',
+			color: '#EE9A37'
+		},
+		{
+			label: 'Verbal communication',
+			avatar: undefined,
+			color: '#5cce88'
+		},
+		{
+			label: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+			avatar: undefined,
+			color: '#41545D'
+		},
+		{
+			label: 'Critical thinking & problem solving',
+			avatar: 'assets/images/background-profile-header.jpg',
+			color: '#1D5D93'
+		}
+	];
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+	}
 
 }
