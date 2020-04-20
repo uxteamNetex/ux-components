d14ac8cefc5addcc7a63cff32c6781fdd0416803
Jaime Alonso Fernández <jaime.alonso@netexlearning.com>
2020-04-20 10:40:18 +0200

    Refactorizado componente 'ntx-card10'

--- projects/netex/ux-components/src/lib/ntx-cards/card10/card10.component.ts
+++ projects/netex/ux-components/src/lib/ntx-cards/card10/card10.component.ts
@@ -2,16 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'ntx-card10',
-  templateUrl: './card10.component.html',
-  styleUrls: ['./card10.component.scss'],
-  encapsulation: ViewEncapsulation.None
+	selector: 'ntx-card10',
+	templateUrl: './card10.component.html',
+	styleUrls: ['./card10.component.scss']
 })
 export class Card10Component implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
