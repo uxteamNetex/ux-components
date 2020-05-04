9f7a13b4585a3f4c1283b6a9428c020fc4f6e06e
ja10ns0 <alonsofernandezjaime@gmail.com>
2020-05-04 21:56:51 +0200

    Documentación componente 'ntx-menu-link'

--- src/app/ui-pages/menus/documentation/menus-doc.component.ts
+++ src/app/ui-pages/menus/documentation/menus-doc.component.ts
@@ -134,6 +134,58 @@ code2 = `
 
       }`;
 
+  code5=`
+    <a class="menu-link" [matMenuTriggerFor]="menuOptions">
+    <mat-icon class="icon-netex {{icon}} utils--margin-right-12"></mat-icon>
+    <span>{{title}}</span>
+    </a>
+    <mat-menu #menuOptions="matMenu" [overlapTrigger]="false">
+    <button mat-menu-item (click)="item.click()" mat-menu-item *ngFor="let item of actions">
+      <mat-icon class="mat-icon icon-netex {{item.icon}}"></mat-icon>
+      <span class="utils--text-grey-700">{{item.title}}</span>
+    </button>
+    </mat-menu>
+`;    
+  code6=`
+    import { Component, OnInit, Input } from '@angular/core';
+
+    export class IActionsMenuLinkComponent {
+    icon?: string;
+    title: string;
+    click: Function;
+    }
+
+    @Component({
+    selector: 'ntx-menu-link',
+    templateUrl: './menu-link.component.html',
+    styleUrls: ['./menu-link.component.scss']
+    })
+    export class MenuLinkComponent implements OnInit {
+
+    @Input() title: string;
+    @Input() icon: string;
+    @Input() actions: IActionsMenuLinkComponent[];
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+    }
+  `;    
+  code7=`
+    @import "././projects/netex/ux-components/src/styles/app/utils";
+
+    a.menu-link {
+      color: $color-primary;
+      display: inline-flex;
+      align-items: center;
+      padding: 8px 0;
+      cursor: pointer;
+      &:hover {
+          color: $color-primary-hover;
+      }
+    }
+  `;    
 
 constructor() { }
 
