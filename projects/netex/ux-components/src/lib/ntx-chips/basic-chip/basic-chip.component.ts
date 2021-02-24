413118947f3f2e75b597c1db154adf08ee10bf55
Jaime Alonso Fernández <jaime.alonso@netexlearning.com>
2021-02-24 14:09:27 +0100

    Chips - encapsulation - specific selectors

--- projects/netex/ux-components/src/lib/ntx-chips/basic-chip/basic-chip.component.ts
+++ projects/netex/ux-components/src/lib/ntx-chips/basic-chip/basic-chip.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Input } from '@angular/core';
 import { MatChipInputEvent } from '@angular/material';
 
@@ -11,7 +11,8 @@ export interface Chip {
 @Component({
   selector: 'ntx-basic-chip',
   templateUrl: './basic-chip.component.html',
-  styleUrls: ['./basic-chip.component.scss']
+  styleUrls: ['./basic-chip.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class BasicChipComponent implements OnInit {
 
