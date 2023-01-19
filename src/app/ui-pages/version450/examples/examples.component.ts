0b656571e85b0e2ed58039def9974f75e1408ff2
María Roade Fontenla <maria.roade@netexlearning.com>
2023-01-19 14:33:40 +0100

    Incluir Card25 en el whast new

--- src/app/ui-pages/version450/examples/examples.component.ts
+++ src/app/ui-pages/version450/examples/examples.component.ts
@@ -8,9 +8,22 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class ExamplesComponent implements OnInit {
 
+  card25: any[] = [];
+
   constructor() { }
 
   ngOnInit() {
+
+    this.card25 = [
+			{
+				image: '',
+        avatar: 'assets/images/user_male_01.png',
+        title : 'John Tabalot',
+        subtitle : 'UX Designer - UX Department',
+        description : 'You have been choosen to evaluate John Talabot’s skills. Please select the answers that fits best his level. Please always be honest, this is very important for us as a team!',
+      }
+		];
+
   }
 
 }
